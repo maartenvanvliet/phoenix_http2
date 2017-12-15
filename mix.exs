7ef101518248f7e69cc9491ab2aa54a38f26e5ea
Maarten van Vliet <maartenvanvliet@gmail.com>
2017-12-15 21:41:42 +0100

    Upgrade dependencies to support Cowboy 2

--- mix.exs
+++ mix.exs
@@ -32,12 +32,13 @@ defmodule Http2Test.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, git: "https://github.com/phoenixframework/phoenix", branch: "master", override: true},
+      {:plug, git: "https://github.com/elixir-plug/plug", branch: "master", override: true},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 2.1", override: true},
     ]
   end
 end
